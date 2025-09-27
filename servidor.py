dec8428f8659a146923b4ccd2fea9cd78fe785e4
Carlos David Cruz Cardona <carlos.cruzcqteczion>
2025-09-26 21:12:48 -0600

    se agregaron todas la apis que se usaran

--- servidor.py
+++ servidor.py
@@ -1,1 +1,8 @@
-print("inicio")
+import requests
+from customtkinter import *
+
+url = "https://timeapi.io/api/time/current/zone?timeZone=America%2FGuatemala"  #Cambiamos el URL con la API que requerimos
+url2 = "https://tools.aimylogic.com/api/now?tz=America/Guatemala&format=yyyy-MM-dd"
+url3 = "https://catfact.ninja/facts"
+ciudad = input("Ingresa tu ciudad: ")
+url4 = f"http://wttr.in/{ciudad}?format=j1"
