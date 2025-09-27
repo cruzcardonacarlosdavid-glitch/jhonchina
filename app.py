5b32ddf6aaed40ed0133d0a363f25563428916eb
Abner Toc <abnermoto14@gmail.com>
2025-09-26 22:56:00 -0600

    Me di cuenta que la mayoria de librerias ya estan en el backend.

--- app.py
+++ app.py
@@ -4,8 +4,6 @@
 import customtkinter
 from tkinter import messagebox
 import requests
-import datetime
-import random
 import time
 
 app = customtkinter.CTk()
@@ -13,13 +11,17 @@ app.title("App del Tiempo y Clima")
 app.geometry("500x450")
 
 
-
 def Weather():
-    
 
 
 
 
+
+
+
+
+    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_default_color_theme("blue")
 
 
 
