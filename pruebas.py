8bdb0bad3cb6715604bab68611460132dcc0a6aa
Carlos David Cruz Cardona <carlos.cruzcqteczion>
2025-09-26 22:50:04 -0600

    la api de la fecha esta lista

--- pruebas.py
+++ pruebas.py
@@ -9,4 +9,7 @@ caja = CTkLabel(app, text = "")
 caja.configure(text = f"{hora}:{minutos}" )
 caja.pack()
 
+caja2 = CTkLabel(app, text = "")
+caja2.configure(text = f"{day}/{month}/{year}" )
+caja2.pack()
 app.mainloop()
--- servidor.py
+++ servidor.py
@@ -1,19 +1,27 @@
 import requests
-from customtkinter import *
 
-url = "https://timeapi.io/api/time/current/zone?timeZone=America%2FGuatemala"  #Cambiamos el URL con la API que requerimos
+#api del tiempo
+url = "https://timeapi.io/api/time/current/zone?timeZone=America%2FGuatemala"
 data = requests.get(url)
 hora = data.json()["hour"]
 minutos = data.json()["minute"]
 
+#api de la fecha
 url2 = "https://tools.aimylogic.com/api/now?tz=America/Guatemala&format=yyyy-MM-dd"
+data2  = requests.get(url2)
+year = data2.json()["year"]
+month = data2.json()["month"]
+day = data2.json()["day"]
+
 url3 = "https://catfact.ninja/facts"
 ciudad = input("Ingresa tu ciudad: ")
 url4 = f"http://wttr.in/{ciudad}?format=j1"
 
-def obtener_datos():
 
-    return hora, minutos
+def obtener_datos():
+    return hora, minutos,year,month,day
+
+
 
 
 
