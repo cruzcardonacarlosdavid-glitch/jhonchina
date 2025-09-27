f4629966fde7f5ab21b0265004f78827cb11f3fe
Abner Toc <abnermoto14@gmail.com>
2025-09-26 22:41:31 -0600

    Se agregan las librerias.

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
 #abner esta es tu rama
+#Oh ciedos
+
+import customtkinter
+from tkinter import messagebox
+import requests
+import datetime
+import random
+import time
+
+app = customtkinter.CTk()
+app.title("App del Tiempo y Clima")
+app.geometry("500x450")
+
+
+
+def Weather():
+    
+
+
+
+
+
+
+
+#4 etiquetas y una caja, señal de alerta
