bdd0060f552b98298c4156d62270826b667b00f7
Carlos David Cruz Cardona <carlos.cruzcqteczion>
2025-09-26 23:47:36 -0600

    servidor terminado

--- servidor.py
+++ servidor.py
@@ -12,20 +12,22 @@ data2  = requests.get(url2)
 year = data2.json()["year"]
 month = data2.json()["month"]
 day = data2.json()["day"]
+
+#api de factos
 def factoss():
     url3 = "https://catfact.ninja/fact"
     data3 = requests.get(url3).json()
     facto = data3["fact"]
     return facto
 
+#api de temperatura y la velocidad del viento
 url4 = f"http://wttr.in/solola?format=j1"
 data4 = requests.get(url4).json()
 temp = data4["current_condition"][0]["temp_C"]
-descripcion = data4["current_condition"][0]["weatherDesc"][0]["value"]
 viento = data4["current_condition"][0]["windspeedKmph"]
 
 
 def obtener_datos():
-    return hora, minutos,year,month,day,temp,descripcion,viento
+    return hora, minutos,year,month,day,temp,viento
 
 
