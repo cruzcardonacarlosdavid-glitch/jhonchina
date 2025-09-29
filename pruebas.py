f0992e4b7004e747e281f4db9c3eeb462ee8fdb6
Abner Toc Kaiser <abner.tocb@teczion.edu.gt>
2025-09-29 15:00:08 -0600

    Porque si (2)

--- pruebas.py
+++ pruebas.py
@@ -9,7 +9,7 @@ def factss():
 app = CTk()
 app.title("datos")
 
-fuente = CTkFont(family="Arial",size = 20)
+fuente = CTkFont(family="Arial",size = 50)
 
 caja = CTkLabel(app,text = f"tiempo : {hora} : {minutos}",font = fuente)
 caja.grid(row= 0,column = 0, padx = 5, pady= 5)
@@ -17,13 +17,13 @@ caja.grid(row= 0,column = 0, padx = 5, pady= 5)
 caja2 = CTkLabel(app,text = f"fecha : {day} / {month} / {year}",font = fuente)
 caja2.grid(row= 1,column = 0, padx = 5, pady= 5)
 
-caja3 = CTkLabel(app,text = f"temperatura : {temp}",font = fuente)
+caja3 = CTkLabel(app,text = f"temperatura : {temp} C°",font = fuente)
 caja3.grid(row= 2,column = 0, padx = 5, pady= 5)
 
-caja4 = CTkLabel(app,text = f"velocidad del viento : {viento}",font = fuente)
+caja4 = CTkLabel(app,text = f"velocidad del viento : {viento} km/h",font = fuente)
 caja4.grid(row= 3,column = 0, padx = 5, pady= 5)
 
 
-btn = CTkButton(app,text = "factos",command=factss)
+btn = CTkButton(app,text = "factos",command=factss, font= fuente)
 btn.grid(row= 4,column = 0, padx = 5, pady= 5)
 app.mainloop()
